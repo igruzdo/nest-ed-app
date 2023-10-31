fc0a58ea84f9b35a94da9e26c4ed73614733c104
Igor Gruzdo <igruzdo@yandex.ru>
2023-10-31 18:12:52 +0300

    temporery save

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -1,11 +1,27 @@
-import { CallHandler, ExecutionContext, NestInterceptor } from "@nestjs/common";
+import { CallHandler, ExecutionContext, NestInterceptor, UseInterceptors } from "@nestjs/common";
+import { plainToInstance } from "class-transformer";
 import { Observable, map } from "rxjs";
 
+interface ClassConstructor {
+  new(...args: any[]): {}
+}
+
+export function Serialize(dto: ClassConstructor) {
+  return UseInterceptors(new SerializeInterceptor(dto))
+}
+
 export class SerializeInterceptor implements NestInterceptor {
+
+  constructor(private dto: any) {
+
+  }
+
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     return next.handle().pipe(
       map((data) => {
-        
+        return plainToInstance(this.dto, data, {
+          excludeExtraneousValues: true,
+        })
       })
     )
   }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,9 @@
-import { Body, ClassSerializerInterceptor, Controller, Delete, Get, NotFoundException, Param, Patch, Post, Query, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Delete, Get, NotFoundException, Param, Patch, Post, Query } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user.dto';
+import { UserDto } from './dtos/user.dto';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
 
 @Controller('auth')
 export class UsersController {
@@ -12,7 +14,7 @@ export class UsersController {
     this.usersService.create(body.email, body.password);
   }
 
-  @UseInterceptors(ClassSerializerInterceptor)
+  @Serialize(UserDto)
   @Get('/:id')
   public findUser(@Param('id') id: string) {
     const user = this.usersService.findOne(+id);
