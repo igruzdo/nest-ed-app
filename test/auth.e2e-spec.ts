c6d68798009f36d48a5cbf6772a5124867c8371d
Igor Gruzdo <igruzdo@yandex.ru>
2023-11-17 00:20:02 +0300

    finish tests

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -31,4 +31,21 @@ describe('Auth system', () => {
         expect(email).toEqual(emailSend)
       })
   });
+
+  it('signup as a new user then get the currently logged in user', async () => {
+    const email = 'asdasd@asdfasdf.com'
+
+    const res = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email, password: 'asdas'})
+      .expect(201)
+    const cookie = res.get('Set-Cookie');
+
+    const { body } = await request(app.getHttpServer())
+    .get('/auth/whoami')
+    .set('Cookie', cookie)
+    .expect(200)
+
+    expect(body.email).toEqual(email);
+  })
 });
